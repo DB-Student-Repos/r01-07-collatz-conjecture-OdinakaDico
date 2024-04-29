34a4a41b065d04464e83ba1c81a4cf287e9357da
OdinakaDico <149073751+OdinakaDico@users.noreply.github.com>
2024-04-29 15:44:27 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
+pub fn collatz(n: u64) -> Option<u64> {
 pub fn collatz(mut n: u64) -> Option<u64> {
     if n == 0 {
         return None;
     }
+    
 
     let mut steps = 0;
     while n != 1 {
         if n % 2 == 0 {
-            n /= 2;
-        } else {
+@@ -11,7 +11,10 @@ pub fn collatz(n: u64) -> Option<u64> {
             n = 3 * n + 1;
         }
         steps += 1;
@@ -16,5 +17,6 @@ pub fn collatz(mut n: u64) -> Option<u64> {
             return None;
         }
     }
+
     Some(steps)
 }
